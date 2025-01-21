f14dfc7101d9614619933b56ce7f3668c547f9e7
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-01-21 23:16:06 +0100

    implementation of redux-persist plugin

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux'; // importa un componente apposito per React
-import store from './redux/store/index.js';
+import { store, persistedStore } from './redux/store/index.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistedStore}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,6 +2,13 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import cartReducer from '../reducers/cart';
 import userReducer from '../reducers/user';
 import bookReducer from '../reducers/book';
+import { persistStore, persistReducer } from 'redux-persist';
+import localStorage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  storage: localStorage,
+  key: 'root',
+};
 
 // con combineReducers vogliamo compattare tutte le fette (e tutti i
 // reducers) dentro un'unica "pizza"
@@ -14,9 +21,18 @@ const greatReducer = combineReducers({
   book: bookReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, greatReducer);
+
 const store = configureStore({
   // qua va il reducer dello store di Redux
-  reducer: greatReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
+  },
 });
 
-export default store;
+const persistedStore = persistStore(store);
+
+export { store, persistedStore };
